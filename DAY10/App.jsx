d4f1183eab259ccd12d876cef6bbb25d1752a48b
amishakakadiya704 <amishakakadiya704@gmail.com>
2026-05-21 13:53:45 +0530

    Create App component with Hello_1 import

---
+++ DAY10/App.jsx
@@ -0,0 +1,15 @@
+
+import Hello from "./Hello"
+import Hello_1 from "./Hello_1"
+
+function App() {
+
+  return (
+    <>
+    {/* <Hello /> */}
+    <Hello_1/>
+    </>
+  )
+}
+
+export default App
